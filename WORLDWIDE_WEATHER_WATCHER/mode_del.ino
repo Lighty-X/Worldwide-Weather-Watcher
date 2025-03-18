696ebcf7fa7146543cefcf5bbe19770fea81fa26
Lighty-X <paul.moures78@gmail.com>
2025-03-18 13:55:36 +0100

    mode_del.ino

--- WORLDWIDE_WEATHER_WATCHER/mode_del.ino
+++ WORLDWIDE_WEATHER_WATCHER/mode_del.ino
@@ -1,94 +1,3 @@
-unsigned long prevMs = 0;
-bool colSt = false;
-unsigned long intr[] = {1000, 2000};
-int intIdx = 0;
-
-void errH() {
-  unsigned long curMs = millis();
-  if (curMs - prevMs >= intr[intIdx]) {
-    prevMs = curMs;
-    if (colSt) {
-      ledR();
-    } else {
-      ledB();
-    }
-    colSt = !colSt;
-  }
-}
-
-void errGPS() {
-  unsigned long curMs = millis();
-  if (curMs - prevMs >= intr[intIdx]) {
-    prevMs = curMs;
-    if (colSt) {
-      ledR();
-    } else {
-      ledJ();
-    }
-    colSt = !colSt;
-  }
-}
-
-void carteP() {
-  unsigned long curMs = millis();
-  if (curMs - prevMs >= intr[intIdx]) {
-    prevMs = curMs;
-    if (colSt) {
-      ledR();
-    } else {
-      ledW();
-    }
-    colSt = !colSt;
-  }
-}
-
-void errW() {
-  unsigned long curMs = millis();
-  if (curMs - prevMs >= intr[intIdx]) {
-    prevMs = curMs;
-    if (colSt) {
-      ledR();
-    } else {
-      ledW();
-    }
-    colSt = !colSt;
-    intIdx++;
-    if (intIdx >= sizeof(intr) / sizeof(intr[0])) {
-      intIdx = 0;
-    }
-  }
-}
-
-void errC() {
-  unsigned long curMs = millis();
-  if (curMs - prevMs >= intr[intIdx]) {
-    prevMs = curMs;
-    if (colSt) {
-      ledR();
-    } else {
-      ledV();
-    }
-    colSt = !colSt;
-  }
-}
-
-void dataI() {
-  unsigned long curMs = millis();
-  if (curMs - prevMs >= intr[intIdx]) {
-    prevMs = curMs;
-    if (colSt) {
-      ledR();
-    } else {
-      ledV();
-    }
-    colSt = !colSt;
-    intIdx++;
-    if (intIdx >= sizeof(intr) / sizeof(intr[0])) {
-      intIdx = 0;
-    }
-  }
-}
-
 void ledJ() {
   leds.setColorRGB(0, 255, 255, 0);
 }
@@ -112,3 +21,40 @@ void ledB() {
 void ledO() {
   leds.setColorRGB(0, 255, 30, 0);
 }
+
+void errH() {
+  while (true) {  // Boucle infinie
+    ledR();
+    delay(1000);  // Attendre 1 seconde
+    ledB();
+    delay(1000);  // Attendre 1 seconde
+  }
+}
+/*
+void errGPS() {
+  while (true) {  // Boucle infinie
+    ledR();
+    delay(1000);  // Attendre 1 seconde
+    ledJ();
+    delay(1000);  // Attendre 1 seconde
+  }
+}
+*/
+void carteP() {
+while (true) {  // Boucle infinie
+    ledR();
+    delay(1000);  // Attendre 1 seconde
+    ledW();
+    delay(1000);  // Attendre 1 seconde
+  }
+}
+
+void errC() {
+  while (true) {  // Boucle infinie
+    ledR();
+    delay(1000);  // Attendre 1 seconde
+    ledV();
+    delay(1000);  // Attendre 1 seconde
+  }
+}
+
